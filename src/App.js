f57d394c7b7c1461e79c53e0a3354c059197eae2
NP307 <40033104+np307@users.noreply.github.com>
2022-04-12 13:36:24 +0300

    create playground

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import './aim.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxHeight: window.innerHeight,
+      maxWidth: window.innerWidth,
+      aimBlockHeight: 200,
+      aimBlockWidth: 200,
+
+    }
+  }
+
+  render(){
+    const { maxHeight, maxWidth, aimBlockHeight, aimBlockWidth } = this.state;
+
+    setInterval(() => {
+      this.setState({
+        maxWidth: window.innerWidth - aimBlockWidth,
+        maxHeight: window.innerHeight - aimBlockHeight
+      })
+    });
+
+    const aimBlock = {
+      backgroundColor: 'blue',
+      height: aimBlockHeight,
+      width: aimBlockWidth,
+      transform: 'translate(-' + aimBlockHeight / 2 + 'px, -' + aimBlockWidth / 2 + 'px)',
+    };
+
+    const aimWrapper = {
+      marginTop: aimBlockHeight / 2,
+      marginLeft: aimBlockWidth / 2,
+      width: maxWidth,
+      height: maxHeight,
+    };
+
+    return (
+        <>
+          <div style={aimWrapper}>
+            <div style={aimBlock} />
+          </div>
+        </>
+    );
+  }
 }
 
 export default App;
