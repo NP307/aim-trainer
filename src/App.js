c0fe8571345d47106e458f2b2d1d85d617ee6aa9
NP307 <40033104+np307@users.noreply.github.com>
2022-04-15 15:05:12 +0300

    completed a simplified projectтт

--- src/App.js
+++ src/App.js
@@ -5,32 +5,68 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      maxHeight: window.innerHeight,
-      maxWidth: window.innerWidth,
+      maxHeight: '',
+      maxWidth: '',
       aimBlockHeight: 200,
       aimBlockWidth: 200,
+      aimBlockColor: 'blue',
+      positionX: null,
+      positionY: null,
+      pos: null
+    };
+    this.stopPosition = this.stopPosition.bind(this);
+  }
 
-    }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        maxWidth: window.innerWidth - this.state.aimBlockWidth,
+        maxHeight: window.innerHeight - this.state.aimBlockHeight
+      });
+    });
+    this.startPosition()
+  }
+
+  startPosition() {
+    this.setState({
+      pos: setInterval(() => {
+        this.setState((state) => {
+          state.positionX = Math.round(Math.random() * state.maxWidth);
+          state.positionY = Math.round(Math.random() * state.maxHeight);
+        })
+      }, 1000),
+    });
+  }
+
+  stopPosition() {
+    clearInterval(this.state.pos);
+    this.setState({
+      aimBlockColor: 'red',
+    });
+    setTimeout(() => {
+      this.startPosition();
+      this.setState({
+        aimBlockColor: 'blue',
+      });
+    }, 5000);
+
   }
 
   render(){
-    const { maxHeight, maxWidth, aimBlockHeight, aimBlockWidth } = this.state;
-
-    setInterval(() => {
-      this.setState({
-        maxWidth: window.innerWidth - aimBlockWidth,
-        maxHeight: window.innerHeight - aimBlockHeight
-      })
-    });
+    const { maxHeight, maxWidth, aimBlockHeight, aimBlockWidth, positionX, positionY } = this.state;
 
     const aimBlock = {
-      backgroundColor: 'blue',
+      backgroundColor: this.state.aimBlockColor,
+      position: 'absolute',
       height: aimBlockHeight,
       width: aimBlockWidth,
-      transform: 'translate(-' + aimBlockHeight / 2 + 'px, -' + aimBlockWidth / 2 + 'px)',
+      left: positionX,
+      top: positionY,
+      transform: 'translate(-' + aimBlockWidth / 2 + 'px, -' + aimBlockHeight / 2 + 'px)',
     };
 
     const aimWrapper = {
+      position: 'relative',
       marginTop: aimBlockHeight / 2,
       marginLeft: aimBlockWidth / 2,
       width: maxWidth,
@@ -40,7 +76,7 @@ class App extends React.Component {
     return (
         <>
           <div style={aimWrapper}>
-            <div style={aimBlock} />
+            <div style={aimBlock} onClick={this.stopPosition} />
           </div>
         </>
     );
